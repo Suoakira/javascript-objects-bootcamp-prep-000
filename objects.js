7e29d804526750efa8037beb8cd7915375ad37b2
Stephen Courtier <steves0089@gmail.com>
2018-09-04 13:22:13 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -2,9 +2,11 @@ var playlist = new Object({name: "songs"})
 
 function updatePlaylist(object, name, song) {
   object[name] = song;
+  return object
 } 
 
 function removeFromPlaylist(playlist, artistName) {
   delete playlist[artistName]
+  return playlist
 }
 
